d35f5944758e34bb22de176c4022f6023edfc254
Yash Barde <144440613+yashbarde12@users.noreply.github.com>
2024-01-26 20:56:56 +0530

    Add files via upload

--- orm_and_frontend/product/admin.py
+++ orm_and_frontend/product/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from product.models import ProductTable
-# Register your models here.
 
-admin.site.register(ProductTable)
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+   list_display = ['id','name','price','details','category','is_active','rating','image']
+   
+admin.site.register(ProductTable,ProductAdmin)

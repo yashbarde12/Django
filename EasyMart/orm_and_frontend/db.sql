75b33f531a09bb915d4908f5a630d04a2708909f
Yash Barde <144440613+yashbarde12@users.noreply.github.com>
2024-01-31 09:52:43 +0530

    Add files via upload

---
+++ EasyMart/orm_and_frontend/db.sql
@@ -0,0 +1,13 @@
+create database orm_and_frontend;
+
+use orm_and_frontend;
+
+drop database orm_and_frontend;
+
+show tables;
+
+describe product_producttable;
+
+select * from product_producttable;
+
+delete from product_producttable;
